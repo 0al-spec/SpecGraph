495cc8d6d95ff2038c279accf36772019b0b6dc0
Egor Merkushev <gorkaedeep@gmail.com>
2026-05-13 16:12:38 +0300

    Attach SG-SPEC-0050 trace contract

--- tests/test_supervisor.py
+++ tests/test_supervisor.py
@@ -9130,6 +9130,7 @@ def test_live_spec_evidence_contract_is_chain_complete(
         ("SG-SPEC-0001", []),
         ("SG-SPEC-0002", []),
         ("SG-SPEC-0003", []),
+        ("SG-SPEC-0050", []),
     ],
 )
 def test_live_spec_trace_contract_is_registry_backed(
